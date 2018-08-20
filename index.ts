8cf072a40c46862008231f5c184195e3efc93f34
tao24 <tao24.k@gmail.com>
2018-08-20 22:18:14 +0200

    FEATURE: Validating tasks on tests
    Added validation of tasks on sepcified tests.
    Usage:
    ```gcg <task-name>```

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 import minimist from 'minimist';
 import { Initializer } from './src/init';
-import { validate } from './src/validate';
+import { Validator } from './src/validate';
 import { Interactive } from './src/interactive';
 
 const args: minimist.ParsedArgs = minimist(process.argv.slice(2), {
@@ -18,7 +18,8 @@ if(args._[0] === 'init' || args['init']) {
     const initializer = new Initializer(args);
     initializer.start();
 } else if(args._[0] && args._[0].length == 1) {
-    validate(args);
+    const validator = new Validator(args);
+    validator.start();
 } else {
     const interactive = new Interactive(args);
     interactive.start();
--- src/validate.ts
+++ src/validate.ts
@@ -1,43 +1,72 @@
 import { execFile } from 'child_process';
-import { createReadStream, readdir, readFileSync } from 'fs';
+import { createReadStream, readdir, readFileSync, existsSync } from 'fs';
 import { ParsedArgs } from 'minimist';
 
-export function validate(args: ParsedArgs): void {
+export class Validator {
+    programName: string;
+    programPath: string;
+    sourcePath: string;
 
-}
+    constructor(private args: ParsedArgs) {
+        this.programName = args._[0];
+        this.sourcePath = this.programName + '.cpp';
+        this.programPath = './' + this.programName;
+    }
 
-
-
-function validateProgram(programPath: string, inputPath: string, outputPath: string) {
-    var child = execFile(programPath, (error, stdout, stderr) => {
-        if (error) {
-            console.error("\x1b[31m%s\x1b[0m %s", 'ERROR:', stderr ? stderr : "Could not execute file: " + programPath);
-            return;
-        }
-        const out = readFileSync(outputPath);
-        if(stdout.trim() == out.toString().trim()) {
-            console.log("\x1b[32m%s\x1b[0m", "SUCCESS");
-        } else {
-            console.log("\x1b[31m%s\x1b[0m", "INVALID ANSWER:");
-            console.log(" Expected:", out.toString());
-            console.log(' Got:     ', stdout);
-        }
-    });
-    
-    const streamIn = createReadStream(inputPath);
-    streamIn.pipe(child.stdin);
-}
-
-function checkFile(filename: string) {
-    readdir('./tests', (err, items) => {
-        if(!items) {
-            console.error("\x1b[31m%s\x1b[0m %s", 'ERROR:', "Could not find directory: './tests' or it is empty");
+    start() {
+        if(!existsSync(this.sourcePath)) {
+            console.error("\x1b[31m%s\x1b[0m%s", 'ERROR:', "Could not find file: " + this.sourcePath);
             process.exit();
         }
-        items.forEach(item => {
-            if(item.endsWith('.in') && item.startsWith(filename)) {
-                validateProgram(filename, 'tests/' + item, 'tests/' + item.replace(/\.[^/.]+$/, "") + '.out');
+        if(!this.args.no_compile) {
+            execFile('g++', 
+                ['-std=c++17', this.sourcePath,  '-o', this.programName], (error: Error | null, stdout: string, stderr: string) => {
+                    if(error) {
+                        console.error("\x1b[31m%s\x1b[0m %s", "COMPILE ERROR:", stderr);
+                        process.exit();
+                    }
+                    this.checkFile();
+                });
+        } else {
+            if(!existsSync(this.programName)) {
+                console.error("\x1b[31m%s\x1b[0m %s %s", 'ERROR:', "Could not find file: " + this.programName + '.', "Maybe remove 'no-compile' flag?");
+                process.exit();
+            }
+            this.checkFile();
+        }
+    }
+
+    checkFile() {
+        readdir('./tests', (err, items) => {
+            if(!items) {
+                console.error("\x1b[31m%s\x1b[0m %s", 'ERROR:', "Could not find directory: './tests' or it is empty");
+                process.exit();
+            }
+            items.forEach(item => {
+                if(item.endsWith('.in') && item.startsWith(this.programName)) {
+                    this.validateProgram(this.programPath, 'tests/' + item, 'tests/' + item.replace(/\.[^/.]+$/, "") + '.out');
+                }
+            });
+        });
+    }
+
+    validateProgram(programPath: string, inputPath: string, outputPath: string) {
+        var child = execFile(programPath, (error, stdout, stderr) => {
+            if (error) {
+                console.error("\x1b[31m%s\x1b[0m %s", 'ERROR:', stderr ? stderr : "Could not execute file: " + this.programPath);
+                return;
+            }
+            const out = readFileSync(outputPath);
+            if(stdout.trim() == out.toString().trim()) {
+                console.log("\x1b[32m%s\x1b[0m", "SUCCESS");
+            } else {
+                console.log("\x1b[31m%s\x1b[0m", "INVALID ANSWER:");
+                console.log(" Expected:\n", out.toString());
+                console.log(' Got:     \n', stdout);
             }
         });
-    });
+        
+        const streamIn = createReadStream(inputPath);
+        streamIn.pipe(child.stdin);
+    }
 }
